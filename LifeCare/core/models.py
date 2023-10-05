0abe54e5f03667359cc520ca476307e9319ab008
Cristian <cgalbornoz@gmail.com>
2023-10-04 22:22:29 -0300

    Se agregó la conexión a la BBDD

--- LifeCare/core/models.py
+++ LifeCare/core/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 
 # Create your models here.
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    
